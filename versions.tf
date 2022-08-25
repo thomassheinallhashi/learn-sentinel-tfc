242b401e1e67da99cc6e62f03a65b5905585db50
thomassheinallhashi <110841436+thomassheinallhashi@users.noreply.github.com>
2022-08-25 15:51:12 -0400

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -13,7 +13,7 @@ terraform {
   required_version = ">= 0.15"
 
   backend "remote" {
-    organization = "<YOUR_TERRAFORM_ORG>"
+    organization = "34978-tomstestlab"
 
     workspaces {
       name = "sentinel-example"
